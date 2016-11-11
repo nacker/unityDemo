fc1c3f11533652cb4550cebad6917dc00bd0228a
nacker <648959@qq.com>
2016-11-11 17:17:17 +0800

    EarthRotation init

---
+++ EarthRotation/Assets/EarthCtrl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthCtrl : MonoBehaviour {
+
+    private GameObject earth;
+    private GameObject sun;
+
+
+	void Start () {
+
+        earth = GameObject.Find("Earth");
+        sun = GameObject.Find("Sun");
+
+	}
+	
+
+	void Update () {
+
+        earth.transform.RotateAround(sun.transform.position, Vector3.up, 1.0f);
+
+	}
+}
