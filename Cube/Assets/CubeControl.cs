da07e2de13697b4a12215619b7ae03720082bb02
nacker <648959@qq.com>
2016-11-11 14:53:08 +0800

    Cube init

---
+++ Cube/Assets/CubeControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * 100);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(Vector3.left * Time.deltaTime * 100);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * 100);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime * 100);
+        }
+
+    }
+}
